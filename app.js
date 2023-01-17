ae93db7a4fdd0f36d8befa2f9f26655e6f2d0792
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-01-17 10:28:32 +0600

    Implemented server API

---
+++ app.js
@@ -0,0 +1,47 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { config } from 'dotenv';
+import express from 'express';
+import ErrorMiddleware from './middlewares/Error.js';
+
+config({
+  path: './config/config.env',
+});
+const app = express();
+
+// Using Middlewares
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
+
+// Importing & Using Routes
+import course from './routes/courseRoutes.js';
+import other from './routes/otherRoutes.js';
+import payment from './routes/paymentRoutes.js';
+import user from './routes/userRoutes.js';
+
+app.use('/api/v1', course);
+app.use('/api/v1', user);
+app.use('/api/v1', payment);
+app.use('/api/v1', other);
+
+export default app;
+
+app.get('/', (req, res) =>
+  res.send(
+    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+  )
+);
+
+app.use(ErrorMiddleware);
---
+++ server.js
@@ -0,0 +1,24 @@
+import cloudinary from 'cloudinary';
+import nodeCron from 'node-cron';
+import app from './app.js';
+import { connectDB } from './config/database.js';
+import { Stats } from './models/Stats.js';
+connectDB();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+nodeCron.schedule('0 0 0 5 * *', async () => {
+  try {
+    await Stats.create({});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on port: ${process.env.PORT}`);
+});
