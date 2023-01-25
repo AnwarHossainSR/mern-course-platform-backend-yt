91b48fc2e3db9dc17ff01604ba7cbccf1e2b37ca
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-01-25 09:07:54 +0600

    home route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import {
   addToPlaylist,
   changePassword,
@@ -15,41 +15,45 @@ import {
   updateProfile,
   updateprofilepicture,
   updateUserRole,
-} from "../controllers/userController.js";
-import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
-import singleUpload from "../middlewares/multer.js";
+} from '../controllers/userController.js';
+import { authorizeAdmin, isAuthenticated } from '../middlewares/auth.js';
+import singleUpload from '../middlewares/multer.js';
 
 const router = express.Router();
 
-router.route("/register").post(singleUpload, register);
+router.get('/', (req, res) => {
+  res.send(`API is running...`);
+});
 
-router.route("/login").post(login);
+router.route('/register').post(singleUpload, register);
 
-router.route("/logout").get(logout);
+router.route('/login').post(login);
 
-router.route("/me").get(isAuthenticated, getMyProfile);
+router.route('/logout').get(logout);
 
-router.route("/me").delete(isAuthenticated, deleteMyProfile);
+router.route('/me').get(isAuthenticated, getMyProfile);
 
-router.route("/changepassword").put(isAuthenticated, changePassword);
+router.route('/me').delete(isAuthenticated, deleteMyProfile);
 
-router.route("/updateprofile").put(isAuthenticated, updateProfile);
+router.route('/changepassword').put(isAuthenticated, changePassword);
+
+router.route('/updateprofile').put(isAuthenticated, updateProfile);
 
 router
-  .route("/updateprofilepicture")
+  .route('/updateprofilepicture')
   .put(isAuthenticated, singleUpload, updateprofilepicture);
 
-router.route("/forgetpassword").post(forgetPassword);
-router.route("/resetpassword/:token").put(resetPassword);
+router.route('/forgetpassword').post(forgetPassword);
+router.route('/resetpassword/:token').put(resetPassword);
 
-router.route("/addtoplaylist").post(isAuthenticated, addToPlaylist);
+router.route('/addtoplaylist').post(isAuthenticated, addToPlaylist);
 
-router.route("/removefromplaylist").delete(isAuthenticated, removeFromPlaylist);
+router.route('/removefromplaylist').delete(isAuthenticated, removeFromPlaylist);
 
-router.route("/admin/users").get(isAuthenticated, authorizeAdmin, getAllUsers);
+router.route('/admin/users').get(isAuthenticated, authorizeAdmin, getAllUsers);
 
 router
-  .route("/admin/user/:id")
+  .route('/admin/user/:id')
   .put(isAuthenticated, authorizeAdmin, updateUserRole)
   .delete(isAuthenticated, authorizeAdmin, deleteUser);
 
