da89d3e3a575b3c99def3b6de446173c7e23a0f6
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-01-26 08:26:45 +0600

    remove fil for registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,25 +12,25 @@ import { sendToken } from '../utils/sendToken.js';
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
-  const file = req.file;
+  //const file = req.file;
 
-  if (!name || !email || !password || !file)
+  if (!name || !email || !password)
     return next(new ErrorHandler('Please enter all field', 400));
 
   let user = await User.findOne({ email });
 
   if (user) return next(new ErrorHandler('User Already Exist', 409));
 
-  const fileUri = getDataUri(file);
-  const response = await uploader(fileUri.content, 'avatars');
+  //const fileUri = getDataUri(file);
+  //const response = await uploader(fileUri.content, 'avatars');
 
   user = await User.create({
     name,
     email,
     password,
     avatar: {
-      public_id: response.public_id,
-      url: response.secure_url,
+      public_id: 'response.public_id',
+      url: 'response.secure_url',
     },
   });
 
