7d4e3ce44898e624b7b955695573a6c635785a0d
Restyled.io <commits@restyled.io>
2023-01-26 17:03:45 +0000

    Restyled by clang-format

--- utils/cloudinaryConfig.js
+++ utils/cloudinaryConfig.js
@@ -2,18 +2,17 @@ import cloudinary from 'cloudinary';
 
 export const cloudinaryConfig = () => {
   cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET,
   });
 };
 
 export const uploader = async (path, folder) => {
   return await cloudinary.v2.uploader.upload(path, {
-    folder: folder,
+    folder : folder,
   });
 };
 
-export const destroy = async (public_id) => {
-  return await cloudinary.v2.uploader.destroy(public_id);
-};
+export const destroy = async (
+    public_id) => { return await cloudinary.v2.uploader.destroy(public_id);};
