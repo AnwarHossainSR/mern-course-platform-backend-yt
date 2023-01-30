c72a872a60e2a113135d6b06ea4aa5c842c434d2
Restyled.io <commits@restyled.io>
2023-01-30 09:46:42 +0000

    Restyled by clang-format

--- models/User.js
+++ models/User.js
@@ -5,110 +5,108 @@ import mongoose from 'mongoose';
 import validator from 'validator';
 
 const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please enter your name'],
+  name : {
+    type : String,
+    required : [ true, 'Please enter your name' ],
   },
-  email: {
-    type: String,
-    required: [true, 'Please enter your email'],
-    unique: true,
-    validate: validator.isEmail,
-  },
-
-  password: {
-    type: String,
-    required: [true, 'Please enter your password'],
-    minLength: [6, 'Password must be at least 6 characters'],
-    select: false,
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'user'],
-    default: 'user',
+  email : {
+    type : String,
+    required : [ true, 'Please enter your email' ],
+    unique : true,
+    validate : validator.isEmail,
   },
 
-  subscription: {
-    id: {
-      type: String,
-      required: false,
-    },
-    status: {
-      type: String,
-      required: false,
-    },
-    plan: {
-      type: String,
-      required: false,
-    },
-    plan_name: {
-      type: String,
-      required: false,
-    },
-    customer: {
-      type: String,
-      required: false,
-    },
-    current_period_start: {
-      type: Number,
-      required: false,
-    },
-    current_period_end: {
-      type: Number,
-      required: false,
-    },
+  password : {
+    type : String,
+    required : [ true, 'Please enter your password' ],
+    minLength : [ 6, 'Password must be at least 6 characters' ],
+    select : false,
+  },
+  role : {
+    type : String,
+    enum : [ 'admin', 'user' ],
+    default : 'user',
   },
 
-  avatar: {
-    public_id: {
-      type: String,
-      required: true,
+  subscription : {
+    id : {
+      type : String,
+      required : false,
     },
-    url: {
-      type: String,
-      required: true,
+    status : {
+      type : String,
+      required : false,
+    },
+    plan : {
+      type : String,
+      required : false,
+    },
+    plan_name : {
+      type : String,
+      required : false,
+    },
+    customer : {
+      type : String,
+      required : false,
+    },
+    current_period_start : {
+      type : Number,
+      required : false,
+    },
+    current_period_end : {
+      type : Number,
+      required : false,
     },
   },
 
-  playlist: [
+  avatar : {
+    public_id : {
+      type : String,
+      required : true,
+    },
+    url : {
+      type : String,
+      required : true,
+    },
+  },
+
+  playlist : [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Course',
+      type : mongoose.Schema.Types.ObjectId,
+      ref : 'Course',
     },
   ],
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  createdAt : {
+    type : Date,
+    default : Date.now,
   },
 
-  resetPasswordToken: String,
-  resetPasswordExpire: String,
+  resetPasswordToken : String,
+  resetPasswordExpire : String,
 });
 
-schema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+schema.pre('save', async function(next) {
+  if (!this.isModified('password'))
+    return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-schema.methods.getJWTToken = function () {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: '15d',
+schema.methods.getJWTToken = function() {
+  return jwt.sign({_id : this._id}, process.env.JWT_SECRET, {
+    expiresIn : '15d',
   });
 };
 
-schema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
+schema.methods.comparePassword = async function(
+    password) { return await bcrypt.compare(password, this.password); };
 
-schema.methods.getResetToken = function () {
+schema.methods.getResetToken = function() {
   const resetToken = crypto.randomBytes(20).toString('hex');
 
-  this.resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex');
+  this.resetPasswordToken =
+      crypto.createHash('sha256').update(resetToken).digest('hex');
 
   this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
 
