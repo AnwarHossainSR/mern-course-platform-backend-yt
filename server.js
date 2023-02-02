b557e551b5da32fb56480308c0562bf59b670bad
Restyled.io <commits@restyled.io>
2023-02-02 09:59:25 +0000

    Restyled by prettier

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-import nodeCron from 'node-cron';
+import nodeCron from "node-cron";
 
-import app from './app.js';
-import {connectDB} from './config/database.js';
-import {Stats} from './models/Stats.js';
-import {cloudinaryConfig} from './utils/cloudinaryConfig.js';
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+import { Stats } from "./models/Stats.js";
+import { cloudinaryConfig } from "./utils/cloudinaryConfig.js";
 
 connectDB();
 cloudinaryConfig();
 //'0 0 0 1 * *' - every first day of month
-nodeCron.schedule('0 0 0 1 * *', async () => {
+nodeCron.schedule("0 0 0 1 * *", async () => {
   try {
-    console.log('Cron job is running');
+    console.log("Cron job is running");
     await Stats.create({});
   } catch (error) {
     console.log(error);
   }
 });
 
-app.listen(
-    process.env.PORT,
-    () => { console.log(`Server is working on port: ${process.env.PORT}`); });
+app.listen(process.env.PORT, () => {
+  console.log(`Server is working on port: ${process.env.PORT}`);
+});
