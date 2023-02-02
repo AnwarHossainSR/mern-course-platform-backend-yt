c9ad1a441227a89edd220c665bba976e7a3254b1
Restyled.io <commits@restyled.io>
2023-02-02 10:18:22 +0000

    Restyled by clang-format

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 import express from 'express';
+
 import ErrorMiddleware from './middlewares/Error.js';
 
 config({
-  path: './config/config.env',
+  path : './config/config.env',
 });
 const app = express();
 
 // Using Middlewares
-app.use(express.json({ limit: '50mb' }));
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.json({limit : '50mb'}));
+app.use(express.urlencoded({
+  extended : true,
+}));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-);
+app.use(cors({
+  origin : process.env.FRONTEND_URL,
+  methods : [ 'GET', 'POST', 'PUT', 'DELETE' ],
+}));
 
 // Importing & Using Routes
 import course from './routes/courseRoutes.js';
@@ -37,10 +34,7 @@ app.use('/api/v1', other);
 
 export default app;
 
-app.get('/', (req, res) =>
-  res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
+app.get('/', (req, res) => res.send(`<h1>Site is Working. click <a href=${
+                 process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`));
 
 app.use(ErrorMiddleware);
