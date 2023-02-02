8d2ad2a5106bc6773571c44e65410e32d05bdbf3
Restyled.io <commits@restyled.io>
2023-02-02 15:56:29 +0000

    Restyled by clang-format

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,12 @@
-import { createTransport } from 'nodemailer';
+import {createTransport} from 'nodemailer';
 
 export const sendEmail = async (to, subject, text) => {
   const transporter = createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+    host : process.env.SMTP_HOST,
+    port : process.env.SMTP_PORT,
+    auth : {
+      user : process.env.SMTP_USER,
+      pass : process.env.SMTP_PASS,
     },
   });
 
